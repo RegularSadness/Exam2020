3e43c4f99c4f554f9467f851ac4940258fb6f2ec
RegularSadness <shamanovms@mail.ru>
2021-03-26 18:02:26 +0300

    Done!

--- src/exam/exam.kt
+++ src/exam/exam.kt
@@ -1,6 +1,8 @@
 package exam
 
 import java.io.File
+import java.lang.IllegalArgumentException
+import java.lang.StringBuilder
 
 fun example(inputName: String): Int {
     if ("ultimate question" in File(inputName).readText().toLowerCase()) {
@@ -11,6 +13,46 @@ fun example(inputName: String): Int {
 
 // Пожалуйста добавьте необходимые параметры,
 // тип результата, и измените имя функции (Shift+F6)
-fun myExamTask() {
-    TODO()
+fun pressure(inputName: String, query: String): String {
+    var reader = File(inputName).bufferedReader()
+    var dateWithPressure = mutableListOf<String>()
+    var result = ""
+    var finalResult = ""
+
+    reader.forEachLine {
+        dateWithPressure = it.split(": ", ", ") as MutableList<String>
+    }
+
+    if (!query.split(" ").contains(">") && !query.split(" ").contains("<")) {
+        throw IllegalArgumentException("Invalid Argument.")
+    }
+
+    if (query.split(" ").contains("<")) {
+        for (i in 0 until dateWithPressure.size step 2) {
+            if (i < dateWithPressure.size - 2) {
+                if (dateWithPressure[i + 1].toInt() < dateWithPressure[i + 3].toInt()) {
+                    result = result + dateWithPressure[i] + ": " + dateWithPressure[i + 1] + ", "
+                }
+            }
+        }
+    }
+
+    if (query.split(" ").contains(">")) {
+        for (i in 0 until dateWithPressure.size step 2) {
+            if (i < dateWithPressure.size - 2) {
+                if (dateWithPressure[i + 1].toInt() > dateWithPressure[i + 3].toInt()) {
+                    result = result + dateWithPressure[i] + ": " + dateWithPressure[i + 1] + ", "
+                }
+            }
+        }
+    }
+
+    result.toCharArray()
+    for (i in 0..result.length - 3) {
+        finalResult += result[i]
+    }
+
+    finalResult += "."
+    //println(finalResult)
+    return finalResult
 }
--- test/exam/ExamTest.kt
+++ test/exam/ExamTest.kt
@@ -11,7 +11,16 @@ internal class ExamTest {
     }
 
     @Test
-    fun testMyExamTask() {
-        myExamTask()
+    fun pressureFromDownToUpTest() {
+        val actual = pressure("input/pressure.txt", "x < next")
+        val expected = "01.01: 755, 05.01: 758, 12.01: 765, 10.02: 739."
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun pressureFromUpToDownTest() {
+        val actual = pressure("input/pressure.txt", "x > next")
+        val expected = "20.01: 768, 24.01: 763, 30.01: 756, 2.02: 752, 5.02: 745."
+        assertEquals(expected, actual)
     }
 }
